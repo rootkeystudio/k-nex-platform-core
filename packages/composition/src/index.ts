36edcc1ffb222b9b2f6095a5aaa06d192a8d32f4
executionreverted <canersevince3@gmail.com>
2026-08-27 13:58:51 +0300

    feat: produce verifiable release evidence

--- packages/composition/src/index.ts
+++ packages/composition/src/index.ts
@@ -2,3 +2,4 @@ export * from "./installed-plugin-loader.js";
 export * from "./deterministic-resolver.js";
 export * from "./static-artifact-generator.js";
 export * from "./application-factory.js";
+export * from "./release-evidence.js";
